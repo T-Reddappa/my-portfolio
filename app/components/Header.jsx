b92f4377f38d4801100d553df7175287084b1666
Reddappa <itsme.reddy3001@gmail.com>
2025-01-31 09:19:47 +0530

    added resume file

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -63,8 +63,10 @@ const Header = () => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1.2 }}
-          href="/sample-resume.pdf"
+          href="/reddy-resume.pdf"
+          target="_blank"
           download
+          rel="noopener noreferrer"
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white/90 dark:text-black"
         >
           My Resume <Image src={assets.download_icon} alt="" className="w-4" />
